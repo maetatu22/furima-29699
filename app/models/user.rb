a85bc612c22cea4ebbe5ba8372ba88d7042229d2
maetatu22 <zbn.ex.zz@gmail.com>
2020-09-18 14:39:43 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
+
       with_options presence: true do
          validates :nickname
          validates :last_name, format:  { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
@@ -11,5 +14,5 @@ class User < ApplicationRecord
          validates :first_name_kana, format:  { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
          validates :birthday
       end
-         validates :password,format:  { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: "Password Include both letters and numbers"}
+         validates :password,format:  { with: PASSWORD_REGEX, message: "Password Include both letters and numbers"}
 end
