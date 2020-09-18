b56c6cc44320fd65c6cf3a321178d85ff26dddce
maetatu22 <zbn.ex.zz@gmail.com>
2020-09-18 15:39:24 +0900

    一行にまとめる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permit_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname :last_name :first_name :last_name_kana :first_name_kana :birthday])
   end
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
